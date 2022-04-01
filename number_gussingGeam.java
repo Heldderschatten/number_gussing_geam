5beb8dadcdcbff6119f09e379df8187d739d37e4
Heldderschatten <97028479+Heldderschatten@users.noreply.github.com>
2022-04-01 22:04:05 +0200

    Grunglagen for Task3
    I prepare the basics for a search Algogthiom.

--- number_gussingGeam.java
+++ number_gussingGeam.java
@@ -34,6 +34,42 @@ public class number_gussingGeam {
         int max = scanner.nextInt();
         return max;
     }
+    //craet a pc vs player method
+    private static void pcVsPlayer(){
+        Scanner scanner = new Scanner(System.in);
+        int max = 30;
+        int min = 0;
+        int counter = 0;
+        int think;
+        System.out.println("please think to a number between " + min + " and " + max); 
+        randomNumber = sc.nextInt();
+        System.out.println("I have my random number. You have to guess it.");
+        System.out.println("I have set the range from 1 to " + max);
+        boolean isGuessed = false;
+    
+        while(!isGuessed){
+            counter++;
+            System.out.println("Next guess:");
+            int guess = guessNumber(max, min);   //todo: crate an algorithm to guess the number
+            think =  checkNumber(guess);
+            if(think == 1 ){
+                isGuessed = true;
+            }else if(think == 2){
+                max = guess;
+            }else if(think == 3){
+                min = guess;
+            }else{
+                System.out.println("Something went wrong");
+            }
+        }
+        System.out.println("You have guessed " + counter + " times.");
+    }
+    //the algorithm to guess the number
+    private static int guessNumber(int max, int min){
+        int spock = (max + min) / 2;
+        int guess = min + spock;
+        return guess;
+    }
     //create a random number
     public static int randomNumber(int max){
         randomNumber = (int) (Math.random() * (max-1)) + 1;
@@ -41,24 +77,22 @@ public class number_gussingGeam {
         return randomNumber;
     }
     //crating a method to check if the input is the number is correct number
-    public static boolean checkNumber(int guess){
+    public static int checkNumber(int guess){
         if(guess == randomNumber){
             System.out.println("You won!");
-            return true;
+            return 1;
         }
         else if(guess > randomNumber){
             System.out.println("Your number is too high!");
-            return false;
+            return 2;
         }
         else if(guess < randomNumber){
             System.out.println("Your number is too low!");
-            return false;
+            return 3;
         }
         else{
             System.out.println("You have entered an invalid number!");
-            return false;
+            return 4;
         }
     }
-
-
 }
